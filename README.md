664e3378571c21fcdb9c16086129774cfddce583
niranjan <niranjan.b@shawacademy.com>
2017-10-03 16:31:42 +0530

    flarum shaw extension

---
+++ README.md
@@ -0,0 +1,2 @@
+# flarum-ext-shawTheme
+Shaw Academy theme for Flarum Community Platform
---
+++ bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+ini_set('display_errors', 'On');
+
+use Flarum\Event\ConfigureClientView;
+use Illuminate\Contracts\Events\Dispatcher;
+
+return function (Dispatcher $events) {
+
+    $events->listen(ConfigureClientView::class, function (ConfigureClientView $event) {
+        if ($event->isForum()) {
+            $event->addAssets([
+                __DIR__ . '/js/forum/dist/extension.js',
+                __DIR__ . '/less/forum/extension.less',
+            ]);
+            $event->addBootstrapper('romanzpolski/shawTheme/main');
+        }
+    });
+};
